921b33d0356e74404f37087ac0f51741f6a58f9f
himanshikumawat224 <himanshi.kumawat.12027@gmail.com>
2026-07-23 16:53:48 +0530

    Time: 4 ms (23.08%) | Memory: 9.5 MB (7.73%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         
+        unordered_map<char,char> mp1;
+        unordered_map<char,char> mp2;
+        
         for(int i = 0;i< s.length();i++)
         {
-            for(int j = i+1;j< t.length();j++)
-            {
-                if(s[i]==s[j]&&t[i]!= t[j] || s[i]!= s[j] &&  t[i]==t[j])
-                {
-                    return false;
-                }
-            }
+            if(mp1.find(s[i])!=mp1.end()||mp2.find(t[i])!=mp2.end())
+           {
+              if(mp1[s[i]]!=t[i]|| mp2[t[i]]!=s[i])
+              {
+                 return false;
+              }
+           }
+           else{
+             mp1[s[i]]=t[i];
+             mp2[t[i]]=s[i];
+           }
+             
         }
         return true;
     }
